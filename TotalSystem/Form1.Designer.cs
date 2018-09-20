b2c47b6e295ee43aa1f6da1077fb055e9400f7d5
RapidfireCRH <Rapidfire882007@live.com>
2018-09-20 16:29:43 -0600

    Init commit

--- TotalSystem/Form1.Designer.cs
+++ TotalSystem/Form1.Designer.cs
@@ -28,13 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Get Current Location";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 69);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(210, 96);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(210, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Go to";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 198);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(235, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(86, 22);
+            this.toolStripLabel1.Text = "toolStripLabel1";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(199, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(22, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = ">";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(235, 223);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "#GetValue";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- TotalSystem/Form1.cs
+++ TotalSystem/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,101 @@ namespace TotalSystem
 {
     public partial class Form1 : Form
     {
+        star_st curr;
+        public struct star_st
+        {
+            public int edsm_id;
+            public string name;
+            public long value;
+            public string url;
+        }
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            curr = searchbyname("sol");
+            textBox1.Text = "Enter Cmdr Name";
+            button1.Enabled = false;
+            button2.Enabled = false;
+            toolStripLabel1.Text = "";
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                textBox1.Text = "Enter Cmdr Name";
+                textBox1.ForeColor = Color.DarkGray;
+            }
+            else//Check to make sure that the commander name is correct
+            {
+                string result = new System.Net.WebClient().DownloadString("https://www.edsm.net/api-logs-v1/get-position?commanderName=" + textBox1.Text);
+                if (result.Length < 13 || result.Contains("\"msgnum\":203"))
+                {
+                    toolStripLabel1.Text = "No such commander name.";
+                    textBox1.BackColor = Color.Red;
+                    return;
+                }
+                if (result.Contains("\"system\":null"))
+                {
+                    toolStripLabel1.Text = "Non-public profile.";
+                    textBox1.BackColor = Color.Yellow;
+                    return;
+                }
+                textBox1.BackColor = Color.Green;
+                button1.Enabled = true;
+            }
+        }
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            textBox1.ForeColor = DefaultForeColor;
+            textBox1.BackColor = Color.White;
+            if (textBox1.Text == "Enter Cmdr Name")
+            {
+                textBox1.Text = "";
+            }
+        }
+        public star_st searchbyname(string starname)
+        {
+            string result = new System.Net.WebClient().DownloadString("https://www.edsm.net/api-system-v1/estimated-value?systemName=" + starname);
+            star_st ret = new star_st();
+            ret.name = starname;
+            if (result.IndexOf(",\"estimatedValue\":") == -1)
+            {
+                ret.value = -1;
+                ret.url = "";
+                return ret;
+            }
+            ret.value = Int64.Parse(result.Substring(result.IndexOf(",\"estimatedValue\":") + ",\"estimatedValue\":".Length, result.Length - (result.IndexOf(",\"estimatedValue\":") + ",\"estimatedValue\":".Length) - 1));
+            ret.url = result.Substring(result.IndexOf(",\"url\":") + ",\"url\":".Length, (result.IndexOf(",\"estimatedValue\":") - (result.IndexOf(",\"url\":") + ",\"url\":".Length))).Replace("\\", "");
+            ret.url = ret.url.Substring(1, ret.url.Length - 2);
+            return ret;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string result = new System.Net.WebClient().DownloadString("https://www.edsm.net/api-logs-v1/get-position?commanderName=" + textBox1.Text);
+            curr = searchbyname(result.Substring(result.IndexOf(",\"system\":\"") + ",\"system\":\"".Length, result.IndexOf("\",\"firstDiscover") - (result.IndexOf(",\"system\":\"") + ",\"system\":\"".Length)));
+            textBox2.Text = curr.name + Environment.NewLine + curr.value;
+            button2.Text = "Open " + curr.name;
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (curr.url != "")
+                System.Diagnostics.Process.Start(curr.url);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "Enter Cmdr Name")
+                textBox1_Leave(sender, e);
         }
     }
 }
