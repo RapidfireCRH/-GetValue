3c1e80db4f1df4d8664e7ae7da3df100ce3c7d1d
RapidfireCRH <Rapidfire882007@live.com>
2018-09-21 14:45:11 -0600

    Added timer, api, settings page

--- TotalSystem/Form1.Designer.cs
+++ TotalSystem/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
@@ -35,42 +36,50 @@
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
             this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Location = new System.Drawing.Point(351, 14);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(191, 20);
-            this.textBox1.TabIndex = 1;
+            this.textBox1.Size = new System.Drawing.Size(220, 20);
+            this.textBox1.TabIndex = 4;
             this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
             this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 40);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(210, 23);
-            this.button1.TabIndex = 2;
+            this.button1.TabIndex = 1;
             this.button1.Text = "Get Current Location";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(13, 69);
+            this.textBox2.Location = new System.Drawing.Point(12, 41);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(210, 96);
-            this.textBox2.TabIndex = 3;
+            this.textBox2.TabIndex = 2;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 171);
+            this.button2.Location = new System.Drawing.Point(11, 143);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(210, 23);
-            this.button2.TabIndex = 4;
+            this.button2.TabIndex = 3;
             this.button2.Text = "Go to";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -80,9 +89,9 @@
             this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripLabel1});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 198);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 175);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(235, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(583, 25);
             this.toolStrip1.TabIndex = 4;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -94,27 +103,104 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(199, 11);
+            this.button3.Location = new System.Drawing.Point(504, 172);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(22, 23);
+            this.button3.Size = new System.Drawing.Size(69, 23);
             this.button3.TabIndex = 0;
-            this.button3.Text = ">";
+            this.button3.Text = "Settings >";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(251, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Commander Name";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(254, 40);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(124, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Enable check every ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(373, 37);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(42, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "10";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(416, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "seconds";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(254, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "API Key (Optional)";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(254, 128);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(317, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(254, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(319, 39);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Keep in mind, EDSM allows at a rate of 1 request every 5 seconds\r\nstarting with 7" +
+    "20 allowed requests. Queries less then 10 seconds\r\n will generate errors eventua" +
+    "lly. ";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(235, 223);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(583, 200);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "#GetValue";
+            this.TopMost = true;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
@@ -132,6 +218,14 @@
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripLabel toolStripLabel1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- TotalSystem/Form1.cs
+++ TotalSystem/Form1.cs
@@ -13,7 +13,15 @@ namespace TotalSystem
 {
     public partial class Form1 : Form
     {
-        star_st curr;
+        public struct player_st
+        {
+            public string name;
+            public string api;
+            public bool queries;
+            public int seconds_query;
+            public star_st currloc;
+        }
+        player_st currcmdr;
         public struct star_st
         {
             public int edsm_id;
@@ -28,11 +36,12 @@ namespace TotalSystem
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            curr = searchbyname("sol");
+            currcmdr.currloc = searchbyname("sol");
             textBox1.Text = "Enter Cmdr Name";
             button1.Enabled = false;
             button2.Enabled = false;
             toolStripLabel1.Text = "";
+            settings_handle(settings.open);
         }
 
         private void textBox1_Leave(object sender, EventArgs e)
@@ -44,19 +53,52 @@ namespace TotalSystem
             }
             else//Check to make sure that the commander name is correct
             {
-                string result = new System.Net.WebClient().DownloadString("https://www.edsm.net/api-logs-v1/get-position?commanderName=" + textBox1.Text);
+                string result = new System.Net.WebClient().DownloadString("https://www.edsm.net/api-logs-v1/get-position?commanderName=" + textBox1.Text + (textBox4.Text != "" ? "&apiKey=" + textBox4.Text : ""));
                 if (result.Length < 13 || result.Contains("\"msgnum\":203"))
                 {
-                    toolStripLabel1.Text = "No such commander name.";
-                    textBox1.BackColor = Color.Red;
-                    return;
+                    if (textBox4.Text == "")//api is blank
+                    {
+                        toolStripLabel1.Text = "No such commander name.";
+                        textBox1.BackColor = Color.Red;
+                        settings_handle(settings.open);
+                        return;
+                    }
+                    else
+                    {
+                        toolStripLabel1.Text = "Invalid API Key";
+                        textBox1.BackColor = Color.Red;
+                        settings_handle(settings.open);
+                        return;
+                    }
                 }
-                if (result.Contains("\"system\":null"))
+                if (result.Contains("\"system\":null") && textBox4.Text == "")
                 {
                     toolStripLabel1.Text = "Non-public profile.";
                     textBox1.BackColor = Color.Yellow;
+                    settings_handle(settings.open);
                     return;
                 }
+                currcmdr.name = textBox1.Text;
+                currcmdr.queries = checkBox1.Checked;
+                try
+                {
+                    if (currcmdr.queries)
+                    {
+                        int temp = Int32.Parse(textBox3.Text);
+                        if (temp < 1 || temp > 65535)
+                            throw new ArgumentOutOfRangeException();
+                        currcmdr.seconds_query = temp;
+                        timer1.Interval = currcmdr.seconds_query * 1000;
+                    }
+
+                }
+                catch
+                {
+                    currcmdr.seconds_query = 10;
+                    toolStripLabel1.Text = "Error reading seconds, make sure its a value between 1 and 65535.";
+                    timer1.Interval = currcmdr.seconds_query * 1000;
+                }
+                timer1.Enabled = currcmdr.queries;
                 textBox1.BackColor = Color.Green;
                 button1.Enabled = true;
             }
@@ -92,22 +134,70 @@ namespace TotalSystem
         private void button1_Click(object sender, EventArgs e)
         {
             string result = new System.Net.WebClient().DownloadString("https://www.edsm.net/api-logs-v1/get-position?commanderName=" + textBox1.Text);
-            curr = searchbyname(result.Substring(result.IndexOf(",\"system\":\"") + ",\"system\":\"".Length, result.IndexOf("\",\"firstDiscover") - (result.IndexOf(",\"system\":\"") + ",\"system\":\"".Length)));
-            textBox2.Text = curr.name + Environment.NewLine + curr.value;
-            button2.Text = "Open " + curr.name;
+            currcmdr.currloc = searchbyname(result.Substring(result.IndexOf(",\"system\":\"") + ",\"system\":\"".Length, result.IndexOf("\",\"firstDiscover") - (result.IndexOf(",\"system\":\"") + ",\"system\":\"".Length)));
+            if (currcmdr.currloc.value == -1)
+                textBox2.Text = currcmdr.currloc.name + Environment.NewLine + "Error in determining value. API overloaded?";
+            else
+                textBox2.Text = currcmdr.currloc.name + Environment.NewLine + currcmdr.currloc.value;
+            button2.Text = "Open " + currcmdr.currloc.name;
             button2.Enabled = true;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (curr.url != "")
-                System.Diagnostics.Process.Start(curr.url);
+            if (currcmdr.currloc.url != "")
+                System.Diagnostics.Process.Start(currcmdr.currloc.url);
         }
 
+        enum settings { open, closed };
+
         private void button3_Click(object sender, EventArgs e)
-        {
-            if (textBox1.Text != "Enter Cmdr Name")
+        {            
+            if (button3.Text == " Settings>")//closed
+            {
+                settings_handle(settings.open);
+                return;
+            }
+            //Open
+            settings_handle(settings.closed);
+            if (textBox1.Text != "Enter Cmdr Name") //rerun checks to see if there are any changes
                 textBox1_Leave(sender, e);
+        }
+        private void settings_handle(settings state)
+        {
+            if (state == settings.open)
+            {
+
+                ActiveForm.Size = ActiveForm.MaximumSize = ActiveForm.MinimumSize = new Size(599, 239);
+                button3.Location = new Point(504, 172);
+                textBox1.Enabled = textBox3.Enabled = textBox4.Enabled = checkBox1.Enabled = true;
+                button3.Text = "<Settings ";
+            }
+            else
+            {
+                ActiveForm.Size = ActiveForm.MaximumSize = ActiveForm.MinimumSize = new Size(251, 239);
+                button3.Location = new Point(152, 172);
+                textBox1.Enabled = textBox3.Enabled = textBox4.Enabled = checkBox1.Enabled = false;
+                button3.Text = " Settings>";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(!checkBox1.Checked)
+            {
+                timer1.Enabled = false;
+                return;
+            }
+            try
+            {
+                timer1.Interval = currcmdr.seconds_query * 1000;
+            }
+            catch
+            {
+                timer1.Interval = 10000;
+            }
+            button1_Click(sender, e);
         }
     }
 }
